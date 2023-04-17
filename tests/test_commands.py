ec87e7e5e99626c3fe066789371ab40293477493
jonpas <jonpas33@gmail.com>
2023-04-17 03:57:56 +0200

    Fix command none/dry test on Windows when Arma path not found

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -30,5 +30,5 @@ class UnitTests(unittest.TestCase):
         with io.StringIO() as f:
             with contextlib.redirect_stdout(f):
                 ret = armaqdl.main()
-            self.assertEqual(ret, 0)
+            self.assertIn(ret, [0, 2])  # 2 is invalid Arma path
             self.assertTrue("ArmaQDL v" in f.getvalue())
